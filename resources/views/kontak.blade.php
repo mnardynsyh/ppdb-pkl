a7b9ef3970bc89204e13f7bd110b00346b3b2bf5
mnardynsyh <150284930+mnardynsyh@users.noreply.github.com>
2025-09-09 21:56:56 +0700

    add js (kontak)

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -68,7 +68,15 @@
 
 @include('partials.footer')
 
-{{-- Script dan footer sekarang berada di layout utama (app.blade.php) --}}
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+{{-- Menambahkan AlpineJS Collapse Plugin untuk animasi FAQ --}}
+<script src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+<script>
+  AOS.init({
+    duration: 800,
+    once: true
+  });
+</script>
 
 @endsection
 
